5f79f3ccd6639542f37a9b3b7d0e42d3508b6e98
juan3aad <juan3aad@gmail.com>
2021-12-06 17:18:58 -0500

    33 Documentar API con swagger

--- src/main/java/emssanar/salud/web/controller/ProductController.java
+++ src/main/java/emssanar/salud/web/controller/ProductController.java
@@ -6,6 +6,10 @@ import emssanar.salud.domain.Product;
 import java.util.List;
 import java.util.Optional;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 import emssanar.salud.domain.service.ProductService;
+
+import javax.websocket.server.PathParam;
 
 
 @RestController
@@ -26,6 +32,8 @@ public class ProductController {
 	 */
 	//@GetMapping Esta anotacion para exponer los serviciosDELE
 	@GetMapping("/all")// el path es "all"
+	@ApiOperation("Muestra la información de todos los datos")
+	@ApiResponse(code=200, message ="OK")
 	public ResponseEntity<List<Product>> getAll() {
 		return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
 	}
@@ -36,7 +44,12 @@ public class ProductController {
 
 	//----------------
 	@GetMapping("/{id}")
-	public ResponseEntity<Product>getProduct(@PathVariable("id") int productId){
+	@ApiOperation("Searc a product with an ID")
+	@ApiResponses({
+			@ApiResponse(code=200, message = "OK"),
+			@ApiResponse(code=404, message = "Product not found"),
+	})
+	public ResponseEntity<Product>getProduct(@ApiParam(value="The id of the product", required = true, example = "7") @PathVariable("id") int productId){
 		return productService.getProduct(productId)
 				.map(product -> new ResponseEntity<>(product,HttpStatus.OK))
 				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
