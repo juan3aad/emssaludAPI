3cdb8fd6045756a855575e1628d6baae35421a18
juan3aad <juan3aad@gmail.com>
2021-12-28 09:50:35 -0500

    Se adiciona el constructor a la clase AuthenticationResponse

--- src/main/java/emssanar/salud/domain/dto/AuthenticationResponse.java
+++ src/main/java/emssanar/salud/domain/dto/AuthenticationResponse.java
@@ -4,6 +4,10 @@ public class AuthenticationResponse {
 
     private String jwt;
 
+    public AuthenticationResponse(String jwt){
+        this.jwt=jwt;
+    }
+
     public String getJwt() {
         return jwt;
     }
